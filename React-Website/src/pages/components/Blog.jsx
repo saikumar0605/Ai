ddbc5a1c675762e2fc257e65be87d11796a552ba
Abhinesh <abhinesh.on@gmail.com>
2024-02-27 13:17:26 +0530

    blog update

--- React-Website/src/pages/components/Blog.jsx
+++ React-Website/src/pages/components/Blog.jsx
@@ -1,9 +1,63 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import image from "../../assets/image.png";
 
 function Blog() {
+  let articleBody;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getdata();
+  }, [])
+
+  async function getdata() {
+    try {
+      const response = await axios.get("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@vinodkumargr");
+      console.log(response.data)
+      setArticles(response.data.items)
+    } catch (err) {
+      console.error("failed to fetch the data", err);
+    }
+  }
+
+
   return (
-    <div className='flex-1'>Blog</div>
+    <div className='flex-1'>
+      <h1 className='text-center text-4xl font-bold py-4'>Go Through my Blogs!</h1>
+
+      <div>
+        {
+          articles.map((article, index) => {
+            articleBody = article.description
+            return (
+
+
+              <div className='flex flex-col md:flex-row p-9   shadow-xl bg-stone-200 hover:bg-stone-300 hover:border-4 border-stone-200 hover:border-stone-300 rounded-md m-4'>
+                <div className='md:w-2/4'>
+                  <img src={article.thumbnail} className='w-auto rounded-md' />
+                </div>
+                <div className=' md:w-3/4 md:pl-10'>
+                  <h1 className='text-xl pb-6 '>{article.title}</h1>
+                  <p className=' max-h-96 overflow-hidden'> {article.content}
+                  </p>
+                  <div className='flex flex-row-reverse'>
+                    <a target='_blank' rel='noreferrer' href={article.link} key={index} className=' my-4'>
+                      <button className='bg-stone-800 p-3 rounded-md text-stone-200'>Read Blog</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+          )
+
+        }
+
+      </div>
+
+    </div>
   )
 }
 
 export default Blog
+
--- React-Website/src/pages/components/Youtube.jsx
+++ React-Website/src/pages/components/Youtube.jsx
@@ -24,7 +24,7 @@ function Youtube() {
       <h1 className='text-center text-4xl font-bold py-4'>Checkout my Youtube Channel</h1>
       <div className='text-black flex flex-wrap justify-center p-1'>
         {videos.map((vidData) => (
-          <a target='_blank' rel='noreferrer' href={vidData.youtubeId} key="" className="max-w-72 border-white rounded overflow-hidden shadow-lg m-3 bg-stone-200 hover:bg-white hover:border-4">
+          <a target='_blank' rel='noreferrer' href={vidData.youtubeId} key="" className="max-w-72 border-stone-300 rounded overflow-hidden shadow-lg m-3 bg-stone-200 hover:bg-stone-300 hover:border-4">
             <img className="w-full" src={vidData.thumbnail} alt='title' />
             <div className="px-6 py-4">
               <p className="text-gray-700 text-base">{vidData.title}</p>
